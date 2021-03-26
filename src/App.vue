a7a03b2afd1a50d31f5628d236510837114f30c1
Willx096 <guillem.lorca96@gmail.com>
2021-03-26 20:35:41 +0100

    primerasubida

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Task />
+</template>
+
+<script>
+import Task from './components/Task'
+
+export default {
+  name: 'App',
+  components: {
+    Task
+  }
+}
+</script>
+
+<style>
+
+</style>
